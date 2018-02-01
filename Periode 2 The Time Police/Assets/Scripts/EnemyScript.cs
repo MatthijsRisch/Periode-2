b475a1a4f55b1599a3faa6af4be5c3c81cded4ad
MatthijsRisch <matthijs_r555@hotmail.com>
2018-02-01 11:04:06 +0100

    Script opgeschoond
    script opgeschoont

--- Periode 2 The Time Police/Assets/Scripts/EnemyScript.cs
+++ Periode 2 The Time Police/Assets/Scripts/EnemyScript.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
     public Transform player;
-    public int MoveSpeed = 4;
-    public int MaxDist = 1;
-    public int MinDist = 1;
+    public int moveSpeed = 4;
+    public int maxDist = 1;
+    public int minDist = 1;
     public bool start;
 
     // Use this for initialization
@@ -14,14 +14,13 @@ public class EnemyScript : MonoBehaviour {
 		
 	}
 	
-	// Update is called once per frame
 	void FixedUpdate () {
 
         if (start)
         {
             transform.LookAt(player);
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-            if (Vector3.Distance(transform.position, player.position) >= MinDist)
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            if (Vector3.Distance(transform.position, player.position) >= minDist)
             {
 
                 
